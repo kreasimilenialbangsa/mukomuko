73a425538cd5e3f8f2070430be2fa1ae681ab4c1
Budianto <budiianto24@gmail.com>
2022-02-17 10:45:03 +0700

    update gallery

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
     public function index()
     {
-        return view('pages.gallery.index');
+        $galleries = Gallery::select('id', 'title', 'type', 'content', 'created_at')
+            ->whereIsActive(1)
+            ->orderBy('id', 'desc')
+            ->paginate(12);
+
+        return view('pages.gallery.index')
+            ->with('galleries', $galleries);
     }
 }
--- resources/views/pages/gallery/index.blade.php
+++ resources/views/pages/gallery/index.blade.php
@@ -14,29 +14,27 @@
         <section class="col-12 sec-gallery">
           <h4 class="text-center">Galeri</h4>
           <div class="row mt-4">
-            <a class="wblock p-3 col-md-4 col-sm-6 col-12" data-fancybox="video-gallery" data-src="https://www.youtube.com/watch?v=PMUt0lShZfs">
-              <img class="slider-img" src="https://i3.ytimg.com/vi/PMUt0lShZfs/maxresdefault.jpg">
-              <img class="icon-play" src="{{ asset('img/play.svg') }}" height="30" width="30" alt="" class="icon-play">
-              <div class="slider-detail">
-                <p>qweqwe</p>
-              </div>
-            </a>
-            <a class="wblock p-3 col-md-4 col-sm-6 col-12" data-fancybox="video-gallery" data-src="https://www.youtube.com/watch?v=PMUt0lShZfs">
-              <img class="slider-img" src="https://i3.ytimg.com/vi/PMUt0lShZfs/maxresdefault.jpg">
-              <img class="icon-play" src="{{ asset('img/play.svg') }}" height="30" width="30" alt="" class="icon-play">
-              <div class="slider-detail">
-                <p>qweqwe</p>
-              </div>
-            </a>
+            @foreach($galleries as $gallery)
+              @if(@$gallery->type == 'image')
+                <a class="wblock p-3 col-md-4 col-sm-6 col-12" data-fancybox="video-gallery" data-src="{{ asset('storage' . $gallery->content) }}">
+                  <img class="slider-img" src="{{ asset('storage' . $gallery->content) }}">
+                  <div class="slider-detail">
+                    <p>{{ $gallery->title }}</p>
+                  </div>
+                </a>
+              @else
+                <a class="wblock p-3 col-md-4 col-sm-6 col-12" data-fancybox="video-gallery" data-src="https://www.youtube.com/watch?v={{ $gallery->content }}">
+                  <img class="slider-img" src="https://i3.ytimg.com/vi/{{ $gallery->content }}/maxresdefault.jpg">
+                  <img class="icon-play" src="{{ asset('img/play.svg') }}" height="30" width="30" alt="" class="icon-play">
+                  <div class="slider-detail">
+                    <p>{{ $gallery->title }}</p>
+                  </div>
+                </a>
+              @endif
+            @endforeach
           </div>
           <div class="d-flex mt-4 justify-content-center">
-            <ul class="pagination mb-0">
-              <li class="page-item"><a class="page-link" href="#">Prev</a></li>
-              <li class="page-item active"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
+            {{ $galleries->links('vendor.pagination.bootstrap-4') }}
           </div>
         </section>
       </div>
