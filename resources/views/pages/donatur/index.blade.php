11da373e57193dc7f02e41ad5ab60e947d314d6a
Sherly Anjelina <sherlynjelina@gmail.com>
2022-04-25 01:02:44 +0700

    ok

--- resources/views/pages/donatur/index.blade.php
+++ resources/views/pages/donatur/index.blade.php
@@ -14,47 +14,47 @@
           <h4 class="text-center">Donatur</h4>
           <form class="d-center mt-md-5 mt-4 row justify-content-md-between">
             <div class="col-md-6 col-12">
-            <div class="group-select row">
-              {{-- <div class="btn btn-green ml-3 col-md-3 col">
-                <select class="form-control select-cat">
-                  <option selected>Pilih Kecamatan</option>
-                  @foreach($kecamatan as $row)
-                    <option value="{{ $row->id }}">{{ $row->name }}</option>
-                  @endforeach
-                </select>
-              </div> 
-              <div class="btn btn-green mx-2 col-md-3 col">
-                <select class="form-control select-cat">
-                  <option selected>Pilih Program</option>
-                  @foreach($programs as $row)
+              <div class="group-select row">
+                {{-- <div class="btn btn-green ml-3 col-md-3 col">
+                  <select class="form-control select-cat">
+                    <option selected>Pilih Kecamatan</option>
+                    @foreach($kecamatan as $row)
+                      <option value="{{ $row->id }}">{{ $row->name }}</option>
+                    @endforeach
+                  </select>
+                </div> 
+                <div class="btn btn-green mx-2 col-md-3 col">
+                  <select class="form-control select-cat">
+                    <option selected>Pilih Program</option>
+                    @foreach($programs as $row)
+                      <option value="{{ $row->id }}">{{ $row->title }}</option>
+                    @endforeach
+                  </select>
+                </div>
+                <div class="btn btn-green mr-3 col-md-3 col">
+                  <select class="form-control select-cat">
+                    <option selected>Pilih Ziwaf</option>
+                    @foreach($ziswaf as $row)
                     <option value="{{ $row->id }}">{{ $row->title }}</option>
                   @endforeach
-                </select>
-              </div>
-              <div class="btn btn-green mr-3 col-md-3 col">
-                <select class="form-control select-cat">
-                  <option selected>Pilih Ziwaf</option>
-                  @foreach($ziswaf as $row)
-                  <option value="{{ $row->id }}">{{ $row->title }}</option>
-                @endforeach
-                </select>
-              </div>--}}
-              <div class="btn btn-green ml-3 col-md-3 col">
-                <select class="form-control select-cat">
-                  <option selected>Terbaru</option>
-                  <option value="latest">Terlama</option>
-                </select>
+                  </select>
+                </div>--}}
+                <div class="btn btn-green mx-3 col-md-3 col">
+                  <select class="form-control select-cat">
+                    <option selected>Terbaru</option>
+                    <option value="latest">Terlama</option>
+                  </select>
+                </div>
               </div>
             </div>
-          </div>
-          {{-- <div class="col-md-4 col-lg-3 col-12 mt-3">
-            <div class="custom-search">
-              <input type="search" class="input-search w-100" name="search" placeholder="Cari">
-              <button class="btn btn-search btn-green">
-                <ion-icon name="search-outline"></ion-icon>
-              </button>
-            </div>
-          </div> --}}
+            {{-- <div class="col-md-4 col-lg-3 col-12 mt-3">
+              <div class="custom-search">
+                <input type="search" class="input-search w-100" name="search" placeholder="Cari">
+                <button class="btn btn-search btn-green">
+                  <ion-icon name="search-outline"></ion-icon>
+                </button>
+              </div>
+            </div> --}}
           </form>
         </section>
         <section class="col-12 sec-todonation">
