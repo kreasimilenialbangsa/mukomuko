4263a1ec9e74f2041bfe3f59ccce210112398c9a
Sherly Anjelina <sherlynjelina@gmail.com>
2022-04-29 22:41:09 +0700

    ok

--- resources/views/pages/payment/index.blade.php
+++ resources/views/pages/payment/index.blade.php
@@ -14,8 +14,8 @@
             <div class="d-flex">
               <img width="26" height="26" src="{{ asset('img/rp-icon.svg') }}" alt="">
               <div class="title-zakat ml-2">
-                <h3 class="font-medium">Pembayaran {{ isset($donate->title) ? $donate->title : 'Donasi' }}</h3>
-                <p>Isi jumlah donasi dan temukan kebahagianmu sebentar lagi</p>
+                <h4 class="font-medium">Pembayaran {{ isset($donate->title) ? $donate->title : 'Donasi' }}</h4>
+                <p class="text-base clr-grey">Isi jumlah donasi dan temukan kebahagianmu sebentar lagi </p>
               </div>
             </div>
             <label class="text-md font-medium">Jumlah Donasi</label>
@@ -31,14 +31,15 @@
             </div>
             <div class="form-group">
               <input type="text" placeholder="Nama Lengkap" class="form-control" name="name" value="{{ @session()->get('user')['name'] }}">
-              <div class="form-check form-check-inline mt-2">
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="1" name="is_anonim">
-                <label class="form-check-label" for="inlineCheckbox1">Sembunyikan nama saya (Hamba Allah)</label>
-              </div>
+              <label class="custom-check-radio mt-2">
+                Sembunyikan nama saya (Hamba Allah)
+                <input type="checkbox" value="1" name="is_anonim">
+                <span class="checkmark"></span>
+              </label>
             </div>
             <div class="form-group">
               <input type="email" placeholder="Email" class="form-control" name="email" value="{{ @session()->get('user')['email'] }}">
-              <span>Kirim bukti donasi dan kabar perkembangan program melalui email saya.</span>
+              <span class="clr-grey mt-2 d-block">Kirim bukti donasi dan kabar perkembangan program melalui email saya.</span>
             </div>
             <div class="form-group">
               <input type="number" placeholder="Nomor Telepon" class="form-control" name="phone" value="{{ @session()->get('user')['phone'] }}">
@@ -57,50 +58,101 @@
             <div class="d-flex">
               <img width="26" height="26" src="{{ asset('img/rp-icon.svg') }}" alt="">
               <div class="title-zakat ml-2">
-                <h3 class="font-medium">Metode Pembayaran</h3>
-                <p>Pilih metode pembayaran dibawah ini untuk melanjutkan Donasi</p>
+                <h4 class="font-medium">Metode Pembayaran</h4>
+                <p class="text-base clr-grey">Pilih metode pembayaran dibawah ini untuk melanjutkan Donasi</p>
               </div>
             </div>
-            <div class="accordion" id="accordionExample">
+            <div class="accordion method-payment" id="accordionExample">
               <div class="card">
                 <div class="card-header" id="headingOne">
                   <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                       Dompet Digital <br>
-                      Pembayaran dengan dompet digital.
+                      <span class="clr-grey">Pembayaran dengan dompet digital.<span>
                     </button>
                   </h2>
                 </div>
                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                   <div class="card-body">
-                    Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
+                    <div class="d-center item-payment mb-3">
+                      <div class="d-center mr-3">
+                        <label class="custom-check-radio">
+                          <input type="radio" name="payment">
+                          <span class="checkmark"></span>
+                        </label>
+                        <img class="logo-payment" src="{{ asset('img/dana.png') }}" alt="">
+                      </div>
+                      <div class="detail">
+                        <h6>Dana</h6>
+                        <p class="mb-0">Pembayaran melalui digital dana</p>
+                      </div>
+                    </div>
+                    <div class="d-center item-payment mb-3">
+                      <div class="d-center mr-3">
+                        <label class="custom-check-radio">
+                          <input type="radio" name="payment">
+                          <span class="checkmark"></span>
+                        </label>
+                        <img class="logo-payment" src="{{ asset('img/dana.png') }}" alt="">
+                      </div>
+                      <div class="detail">
+                        <h5>Dana</h5>
+                        <p class="mb-0">Pembayaran melalui digital dana</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="card">
                 <div class="card-header" id="headingTwo">
                   <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                       Virtual Account <br>
-                      Bayar di ATM atau Internet Banking
+                      <span  class="clr-grey">Bayar di ATM atau Internet Banking </span>
                     </button>
                   </h2>
                 </div>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                   <div class="card-body">
-                    Some placeholder content for the second accordion panel. This panel is hidden by default.
+                    <div class="d-center item-payment mb-3">
+                      <div class="d-center mr-3">
+                        <label class="custom-check-radio">
+                          <input type="radio" name="payment">
+                          <span class="checkmark"></span>
+                        </label>
+                        <img class="logo-payment" src="{{ asset('img/dana.png') }}" alt="">
+                      </div>
+                      <div class="detail">
+                        <h6>Dana</h6>
+                        <p class="mb-0">Pembayaran melalui digital dana</p>
+                      </div>
+                    </div>
+                    <div class="d-center item-payment mb-3">
+                      <div class="d-center mr-3">
+                        <label class="custom-check-radio">
+                          <input type="radio" name="payment">
+                          <span class="checkmark"></span>
+                        </label>
+                        <img class="logo-payment" src="{{ asset('img/dana.png') }}" alt="">
+                      </div>
+                      <div class="detail">
+                        <h5>Dana</h5>
+                        <p class="mb-0">Pembayaran melalui digital dana</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="form-group">
-            <div class="form-check form-check-inline mt-3">
-              <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-              <label class="form-check-label" for="inlineCheckbox1">Saya setuju dengan syarat dan ketentuan yang berlaku</label>
-            </div>
+          <div class="form-group mt-3">
+            <label class="custom-check-radio">
+              Saya setuju dengan syarat dan ketentuan yang berlaku
+              <input type="checkbox" value="option1">
+              <span class="checkmark"></span>
+            </label>
           </div>
-          <div class="form-group">
+          <div class="form-group mt-4">
             <button class="btn py-2 btn-green w-100">Lanjut Pembayaran</button>
           </div>
         </div>
