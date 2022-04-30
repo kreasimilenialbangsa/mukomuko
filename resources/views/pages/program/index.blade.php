6a77f93a8a9bfdaf672b310096feba0eff2068e7
Sherly Anjelina <sherlynjelina@gmail.com>
2022-04-30 13:57:59 +0700

    ok

--- resources/views/pages/program/index.blade.php
+++ resources/views/pages/program/index.blade.php
@@ -44,45 +44,45 @@
             @forelse($programs as $key => $program)
               <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-3">
                 <div class="card-thumbnail">
-                  <a href="{{ route('program.detail', $program->slug) }}">
-                  <div class="thumb-pict">
-                    <img class="w-100" src="{{ asset('storage/' . $program->image) }}" alt="{{ $program->title }}">
-                  </div>
-                  <div class="card-detail">
-                    <div class="wrap-tags mb-2">
-                      @if($program->is_urgent == 1)
-                        <span class="tag-cat urgent">Darurat</span>
-                      @endif
-                      <span class="tag-cat">{{ $program->category->name }}</span>
+                  <a class="wblock" href="{{ route('program.detail', $program->slug) }}">
+                    <div class="thumb-pict">
+                      <img class="w-100" src="{{ asset('storage/' . $program->image) }}" alt="{{ $program->title }}">
                     </div>
-                    <h6 class="card-title"> 
-                      @if(strlen($program->title) > 100)
-                        {!! substr($program->title, 0, 100) . '...' !!}
+                    <div class="card-detail">
+                      <div class="wrap-tags mb-2">
+                        @if($program->is_urgent == 1)
+                          <span class="tag-cat urgent">Darurat</span>
+                        @endif
+                        <span class="tag-cat">{{ $program->category->name }}</span>
+                      </div>
+                      <h6 class="card-title"> 
+                        @if(strlen($program->title) > 100)
+                          {!! substr($program->title, 0, 100) . '...' !!}
+                        @else
+                          {!! $program->title !!}
+                        @endif
+                      </h6>
+                      <p class="text-xs mb-1 font-medium">{{ $program->location }}</p>
+                      <div class="progress">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: {{ $program->donate_sum_total_donate/$program->target_dana*100 }}%" aria-valuenow="{{ $program->donate_sum_total_donate/$program->target_dana*100 }}" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                      <div class="d-flex mt-2 justify-content-between">
+                        <div class="w-left mr-2">
+                          <span class="text-xs clr-grey">Terkumpul</span>
+                          <h6 class="text-sm">{{ "Rp " . number_format($program->donate_sum_total_donate,0,",",".") }}</h6>
+                        </div>
+                        <div class="w-right text-right">
+                          <span class="text-xs clr-grey">Sisa Hari</span>
+                          <h6 class="text-sm">{{ $program->count_day }}</h6>
+                        </div>
+                      </div>
+                      @if($program->count_day > 0)
+                        <a href="{{ route('program.detail', $program->slug) }}" class="mt-2 py-2 btn btn-green w-100">Ikut Donasi</a>
                       @else
-                        {!! $program->title !!}
+                        <button class="mt-2 py-2 btn btn-secondary w-100" disabled>Ikut Donasi</button>
                       @endif
-                    </h6>
-                    <p class="text-xs mb-1 font-medium">{{ $program->location }}</p>
-                    <div class="progress">
-                      <div class="progress-bar bg-success" role="progressbar" style="width: {{ $program->donate_sum_total_donate/$program->target_dana*100 }}%" aria-valuenow="{{ $program->donate_sum_total_donate/$program->target_dana*100 }}" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <div class="d-flex mt-2 justify-content-between">
-                      <div class="w-left mr-2">
-                        <span class="text-xs clr-grey">Terkumpul</span>
-                        <h6 class="text-sm">{{ "Rp " . number_format($program->donate_sum_total_donate,0,",",".") }}</h6>
-                      </div>
-                      <div class="w-right text-right">
-                        <span class="text-xs clr-grey">Sisa Hari</span>
-                        <h6 class="text-sm">{{ $program->count_day }}</h6>
-                      </div>
-                    </div>
-                    @if($program->count_day > 0)
-                      <a href="{{ route('program.detail', $program->slug) }}" class="mt-2 py-2 btn btn-green w-100">Ikut Donasi</a>
-                    @else
-                      <button class="mt-2 py-2 btn btn-secondary w-100" disabled>Ikut Donasi</button>
-                    @endif
-                  </div>
-                </a>
+                  </a>
                 </div>
               </div>
             @empty
