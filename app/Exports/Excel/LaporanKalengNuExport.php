5101941cadbc83ee6ba4db08c941a34761e3cff8
Budianto <budiianto24@gmail.com>
2022-06-26 07:30:42 +0700

    update report export

--- app/Exports/Excel/LaporanKalengNuExport.php
+++ app/Exports/Excel/LaporanKalengNuExport.php
@@ -3,8 +3,11 @@
 namespace App\Exports\Excel;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class LaporanKalengNuExport implements FromCollection
+class LaporanKalengNuExport implements FromCollection, WithHeadings, WithColumnFormatting
 {
     protected $data;
 
@@ -24,5 +27,34 @@ class LaporanKalengNuExport implements FromCollection
     public function collection()
     {
         return collect($this->data);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Kecamatan',
+            'Desa',
+            'JIPZISNU',
+            'Total Donatur',
+            'Jumlah Donasi',
+            'MUJAMI',
+            'UPZIS RANTING',
+            'UPZIS KEC (MWC)',
+            'LAZISNU',
+            'Jumlah',
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'E' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+        ];
     }
 }
